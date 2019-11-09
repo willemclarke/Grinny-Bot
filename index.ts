16f6effb5d6ce8b34f7bff8063b00d4cdb89ba04
Willem Clarke <willem.2@hotmail.com>
2019-11-09 18:12:18 +1000

    testing things from the discordjs library

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import * as Discord from "discord.js";
 import * as os from "os";
 
 config();
-const token = process.env.DISCORD_TOKEN;
+const token: string = process.env.DISCORD_TOKEN;
 const client = new Discord.Client();
-const sacredId = "524008179861028885";
+const sacredId: string = "524008179861028885";
+const prefix: string = "!";
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -14,8 +15,18 @@ client.once("ready", () => {
 });
 
 client.on("message", message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args: string[] = message.content.slice(prefix.length).split(" ");
+  const command: string = args.shift().toLowerCase();
+
   if (message.content === "!ping") {
     message.channel.send("Pong.");
+  } else if (command === "args-info") {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 });
 
