767189b15f23630b076add2b5e9060be0f009eae
Willem Clarke <willem.2@hotmail.com>
2019-11-12 15:13:59 +1000

    testing heroku deployment

--- index.ts
+++ index.ts
@@ -10,13 +10,16 @@ const faceit = new Faceit(faceitToken);
 
 const discordToken: string = process.env.DISCORD_TOKEN;
 const discord = new Discord.Client();
+// const pitId: string = "642229195405131776";
 const sacredId: string = "524008179861028885";
 const prefix: string = "!";
 
 discord.once("ready", () => {
   console.log("Ready!");
   const sacredChannel = discord.channels.get(sacredId) as Discord.TextChannel;
-  sacredChannel.send(`bot has successfully started up hostname: ${os.hostname}`);
+  sacredChannel.send(
+    `bot has successfully started up hostname: ${os.hostname}`
+  );
 });
 
 discord.on("message", message => {
@@ -28,10 +31,14 @@ discord.on("message", message => {
   if (command === "ping") {
     message.channel.send("Pong.");
   } else if (command === "user-info") {
-    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
   } else if (command === "args-info") {
     if (!args.length) {
-      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
     }
     message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   } else if (command === "stats") {
@@ -39,13 +46,20 @@ discord.on("message", message => {
   }
 });
 
-function runStatsCommand(message: Discord.Message, args: string[]): Promise<Discord.Message | Discord.Message[]> {
+function runStatsCommand(
+  message: Discord.Message,
+  args: string[]
+): Promise<Discord.Message | Discord.Message[]> {
   if (args.length !== 2) {
-    return message.channel.send(`You didn't provide enough arguments: (game, name) are required`);
+    return message.channel.send(
+      `You didn't provide enough arguments: (game, name) are required`
+    );
   } else {
     const [game, username] = args;
     return faceit.getStats(game, username).then(stats => {
-      return message.channel.send(`We received ${JSON.stringify(stats.games, null, 2)}`);
+      return message.channel.send(
+        `We received ${JSON.stringify(stats.games, null, 2)}`
+      );
     });
   }
 }
